14bcf89990fac086d8ec0ee4d672d75e1089b534
jeff-beagley <jeff-beagley@mihlfeld.com>
2018-11-11 23:28:06 -0600

    reworked products page and added images

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,26 +1,24 @@
 <template>
   <div>
-    <h1 class="title">All Products</h1>
-      <p>{{length}} products</p>
-      <table class="table is-striped">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="product in products" track-by="id">
-            <td>{{product.name}}</td>
-            <td>{{product.description}}</td>
-            <td>${{product.price}}</td>
-            <td><button @click='addToCart(product)' class='button is-info'>Add to cart</button></td>
-          </tr>
-        </tbody>
-    </table>
+    <h1 class="title">Store</h1>
+
+    <div v-for="product in products" track-by="id">
+        <div class="row featurette">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">{{product.name}}</h2>
+            <h5>${{product.price}}</h5>
+            <p class="lead">{{product.description}}</p>
+            <b-button @click='addToCart(product)' variant="primary">Add to Cart</b-button>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" src="/static/images/products/cake-1.jpg" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+    </div>
   </div>
+
 </template>
 <script>
 import { mapGetters, mapActions } from 'vuex'
